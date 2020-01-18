d85552b49e9882225fa5bc5a7418950a484d16c6
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-01-18 16:56:28 -0500

    added crud ops  endpoints

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let Todo = require('./todo.model')
 app.use(cors())
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true } )
 const connection = mongoose.connection
 
 connection.once('open', function() {
@@ -27,6 +27,28 @@ todoRoutes.route('/').get(function(req, res) {
             res.json(todos)
         }
     })
+})
+
+todoRoutes.get('/:id', (req, res) => {
+    let id = req.params.id
+    Todo.findById(id, function(err,) {
+        res.json(todo)
+    })
+})
+
+todoRoutes.post('/add', (req,res) => {
+    let todo = new Todo(req.body)
+    todo.save()
+    .then(todo => {
+        res.status(200).json({ 'todo': 'todo added succefully '})
+    })
+    .catch(err =>{
+        res.status(400).send('adding new todo failed')
+    })
+})
+
+todoRoutes.put('/update:id', (req,res) => {
+    let id = req.params.id 
 })
 
 app.use('/todos', todoRoutes)
